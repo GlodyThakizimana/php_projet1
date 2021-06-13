f3d3a5f88f438a943a82dd2bf7c1c6305a2c7755
glodythakizimana <glody.thakizimana@outlook.fr>
2021-06-13 22:09:56 +0200

    💄 styled form

--- formulaire.php
+++ formulaire.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php require('global_css_files.php'); ?>
+    <link rel="stylesheet" href="CSS/formolaire.css">
     <title>Formulaire d'inscription</title>
 </head>
 
@@ -13,19 +14,29 @@
 <body>
     <?php require 'Templates/header.html' ?>
 
-    <legend>Inscription</legend>
+    
 
     <form action="Templates/recup_domainees.php" method="post" >
         <div id="container" >
+            <div class="row">
+                <div class="col"></div>
+                    <legend class="col">Inscription</legend>
+                <div class="col"></div>
+            </div>
             <div class="nom row">
+                <div class="col"></div>
                 <label for="nam" class="col-2">Nom et prénom</label>
                 <input type="text" class="col-3" id="name" name="user_name">
+                <div class="col"></div>
             </div>
             <div class="age row">
+                <div class="col"></div>
                 <label for="tentacles" class="col-2">Age</label>
                 <input type="number" class="col-3" id="tentacles" name="tentacles" min="18" max="100">
+                <div class="col"></div>            
             </div>
             <div class="row">
+                <div class="col"></div>
                 <div class="col-1"></div>
                 <div class="form-check col-3">
                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
@@ -39,13 +50,20 @@
                         Femme
                     </label>
                 </div>
+                <div class="col"></div>
             </div>
             <div class="password row ">
+                <div class="col"></div>
                 <label for="pass" class="col-2">Entrée votre mot de passe (8 caractaire minimume):</label>
                 <input class="col-3" type="password" id="pass" name="password" minlength="8" required>
+                <div class="col"></div>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Valider</button>
+        <div class="row">
+            <div class="col"></div>
+            <button type="submit" class="col-3 btn btn-primary">Valider</button>
+            <div class="col"></div>
+        </div>
     </form>
     </div>
     <?php
